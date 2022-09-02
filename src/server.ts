851ee46bde501762f31107c1075ef3ffc4310d2f
Ahmed Elsadek <elecengahmed@hotmail.com>
2022-09-02 13:47:13 +0200

    Final v03

--- src/server.ts
+++ src/server.ts
@@ -18,7 +18,7 @@ app.use(cors(corsoption));
 app.use(bodyParser.json())
 
 app.get('/', function (req: Request, res: Response) {
-    res.send('Hello World!')
+    res.send('Hello World! -- Ahmed Elsadek ')
 })
 
 Userhandler(app);
